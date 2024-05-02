eb04841260b039ed31c77412b109046157b5f4db
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-03 03:46:29 +0900

    feat: Add global style to apply reset css

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from '@/styles/reset.jsx';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+`;
+
+export default GlobalStyles;
