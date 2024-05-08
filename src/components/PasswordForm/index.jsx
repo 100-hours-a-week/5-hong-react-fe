81eed5ea23cc74fa39bede97e0cde8b2566dd829
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-08 14:17:28 +0900

    feat: Add password form component

---
+++ src/components/PasswordForm/index.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+
+function PasswordForm() {
+  const helperText = '*FIXME';
+
+  return (
+    <StyledForm>
+      <StyledInputContainer>
+        <Input
+          id={'password'}
+          type={'password'}
+          label={'비밀번호'}
+          required={true}
+          placeholder={'이메일을 입력해주세요.'}
+          helperText={helperText}
+        />
+      </StyledInputContainer>
+      <StyledInputContainer>
+        <Input
+          id={'password-confirm'}
+          type={'password'}
+          label={'비밀번호 확인'}
+          required={true}
+          placeholder={'비밀번호를 입력해주세요.'}
+          helperText={helperText}
+        />
+      </StyledInputContainer>
+
+      <Button type={'summit'} text={'수정하기'} width={'100%'} />
+    </StyledForm>
+  );
+}
+
+export default PasswordForm;
+
+const StyledForm = styled.form`
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+const StyledInputContainer = styled.div`
+  margin-top: 8px;
+`;
