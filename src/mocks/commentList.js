6b4caf5944d4aff68a7148ea9214002235e1d94e
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-13 00:51:46 +0900

    chore: Add dummy data for post detail, comments, login

---
+++ src/mocks/commentList.js
@@ -0,0 +1,70 @@
+const commentList = [
+  {
+    commentsId: 6,
+    postsId: 10,
+    contents: '내용내용1',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 1,
+      nickname: '더미사용자1',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+  {
+    commentsId: 5,
+    postsId: 10,
+    contents: '내용내용1',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 2,
+      nickname: '더미사용자2',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+  {
+    commentsId: 4,
+    postsId: 10,
+    contents: '내용내용1',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 1,
+      nickname: '더미사용자1',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+  {
+    commentsId: 3,
+    postsId: 10,
+    contents: '내용내용1',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 1,
+      nickname: '더미사용자1',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+  {
+    commentsId: 2,
+    postsId: 10,
+    contents: '내용내용2',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 2,
+      nickname: '더미사용자2',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+  {
+    commentsId: 1,
+    postsId: 10,
+    contents: '내용내용3',
+    createdAt: '2024-01-01 12:34:56',
+    owner: {
+      memberId: 1,
+      nickname: '더미사용자1',
+      profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+    },
+  },
+];
+
+export default commentList;
---
+++ src/mocks/loginUser.js
@@ -0,0 +1,9 @@
+const loginUser = {
+  memberId: 1,
+  email: 'kinjihong@naver.com',
+  nickname: 'hong.kim',
+  createdAt: '2024-01-01 12:34:56',
+  profileImage: 'https://avatars.githubusercontent.com/u/144337839?v=4',
+};
+
+export default loginUser;
