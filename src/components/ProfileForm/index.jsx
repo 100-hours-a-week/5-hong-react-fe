0d474f3ec39c6fa8b2254dac83234ad46f47f26f
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-14 01:11:42 +0900

    feat: Add nickname validation event into profile form

--- src/components/ProfileForm/index.jsx
+++ src/components/ProfileForm/index.jsx
@@ -1,19 +1,53 @@
 import { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import S from '@/styles/common.jsx';
 import Input from '@/components/Input';
 import Button from '@/components/Button';
 import ImageInput from '@/components/ImageInput';
 import Modal from '@/components/Modal';
+import VALIDATE_MESSAGES from '@/constants/validateMessages.js';
+import REGEX from '@/constants/regex.js';
 
-function ProfileForm() {
+ProfileForm.propTypes = {
+  loginUser: PropTypes.object,
+};
+
+function ProfileForm({ loginUser }) {
   console.debug('ProfileForm() - rendering');
 
-  // TODO: State 추가하면서 전부 수정
-  const email = 'kinjihong9598@gmail.com';
-  const helperText = '*FIXME';
-  const dummyImage = 'https://avatars.githubusercontent.com/u/144337839?v=4';
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [nickname, setNickname] = useState(null);
+  const [helperText, setHelperText] = useState(
+    VALIDATE_MESSAGES.NICKNAME.REQUIRED,
+  );
+
+  const navigate = useNavigate();
+
+  // 닉네임 유효성 검사 이벤트
+  const handleChangeNickname = (e) => {
+    const value = e.target.value;
+
+    if (value.trim().length === 0) {
+      setHelperText(VALIDATE_MESSAGES.NICKNAME.REQUIRED);
+      setNickname(null);
+      setIsDisabled(true);
+      return;
+    }
+
+    if (!REGEX.NICKNAME.test(value)) {
+      setHelperText(VALIDATE_MESSAGES.NICKNAME.INVALID);
+      setNickname(null);
+      setIsDisabled(true);
+      return;
+    }
+
+    setHelperText(null);
+    setNickname(value);
+    setIsDisabled(false);
+  };
 
   // Modal state
   const [isOpen, setIsOpen] = useState(false);
@@ -28,7 +62,7 @@ function ProfileForm() {
     document.body.style.overflow = 'auto'; // 스크롤 이벤트 방지
   }, []);
 
-  const handleConfirm = (e) => {
+  const handleWithdrawButton = (e) => {
     e.preventDefault();
 
     console.log('누름');
@@ -38,7 +72,7 @@ function ProfileForm() {
   const [image, setImage] = useState(null);
 
   useEffect(() => {
-    setImage(dummyImage);
+    setImage(loginUser.profileImage);
   }, []);
 
   const handleImageUpload = (e) => {
@@ -53,6 +87,16 @@ function ProfileForm() {
     reader.onloadend = () => {
       setImage(reader.result);
     };
+  };
+
+  const handleUpdateButton = (e) => {
+    e.preventDefault();
+
+    console.log(`nickname: ${nickname}`);
+  };
+
+  const handleGoMain = () => {
+    navigate('/');
   };
 
   return (
@@ -76,7 +120,7 @@ function ProfileForm() {
           <StyledSubTitle>
             <S.Highlight>이메일</S.Highlight>
           </StyledSubTitle>
-          <StyledSubTitle>{email}</StyledSubTitle>
+          <StyledSubTitle>{loginUser.email}</StyledSubTitle>
         </div>
 
         <div>
@@ -85,11 +129,18 @@ function ProfileForm() {
             type={'text'}
             label={'닉네임'}
             helperText={helperText}
+            onChange={handleChangeNickname}
           />
         </div>
 
         <div>
-          <Button width={'100%'} text={'수정 완료'} type={'summit'} />
+          <Button
+            width={'100%'}
+            text={'수정하기'}
+            type={'submit'}
+            disabled={isDisabled}
+            onClick={handleUpdateButton}
+          />
           <StyledButton type={'button'} onClick={handleOpenModal}>
             회원 탈퇴
           </StyledButton>
@@ -98,8 +149,9 @@ function ProfileForm() {
         <div>
           <Button
             width={'100px'}
-            text={'수정 완료'}
+            text={'수정완료'}
             type={'button'}
+            onClick={handleGoMain}
             $radius={'20px'}
           />
         </div>
@@ -111,7 +163,7 @@ function ProfileForm() {
           title={'회원탈퇴를 하시겠습니까?'}
           contents={'작성하신 게시글과 댓글은 삭제됩니다.'}
           handleClose={handleCloseModal}
-          handleConfirm={handleConfirm}
+          handleConfirm={handleWithdrawButton}
         />
       )}
     </>
