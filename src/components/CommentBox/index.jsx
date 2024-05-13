b824f6623a861cea2324ea81e5ec59b8743eed68
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-13 16:02:08 +0900

    refact: Fit comment components into wrap component

--- src/components/CommentBox/index.jsx
+++ src/components/CommentBox/index.jsx
@@ -1,16 +1,30 @@
+import { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import S from '@/styles/common.jsx';
-import { useCallback, useState } from 'react';
-import Modal from '@/components/Modal/index.jsx';
+import Modal from '@/components/Modal';
 
-function CommentBox() {
-  // 임시
-  const contents = '댓글 내용';
-  const imageUrl = 'https://avatars.githubusercontent.com/u/144337839?v=4';
+CommentBox.propTypes = {
+  id: PropTypes.number,
+  contents: PropTypes.string,
+  createdAt: PropTypes.string,
+  author: PropTypes.object,
+  loginUser: PropTypes.object,
+  onEditClick: PropTypes.func,
+};
 
-  // Modal state
-  const [isOpen, setIsOpen] = useState(false);
+function CommentBox({
+  id,
+  contents,
+  createdAt,
+  author,
+  loginUser,
+  onEditClick,
+}) {
+  console.debug('CommentBox() - rendering');
+
+  const [isOpen, setIsOpen] = useState(false); // 모달 상태
 
   const handleOpenModal = useCallback(() => {
     setIsOpen(true);
@@ -25,22 +39,26 @@ function CommentBox() {
   const handleConfirm = (e) => {
     e.preventDefault();
 
-    console.log('누름');
+    console.log(`댓글 ${id} 삭제 요청`);
   };
 
   return (
     <>
       <CommentInfoContainer>
         <OwnerInfoContainer>
-          <StyledImage src={imageUrl} alt={'OWNER_PROFILE'} />
+          <StyledImage src={author.profileImage} alt={'OWNER_PROFILE'} />
           <p>
-            <S.Highlight>더미 작성자</S.Highlight>
+            <S.Highlight>{author.nickname}</S.Highlight>
           </p>
-          <p>2021-01-01 00:00:00</p>
+          <p>{createdAt}</p>
 
           <ButtonContainer>
-            <StyledButton>수정</StyledButton>
-            <StyledButton onClick={handleOpenModal}>삭제</StyledButton>
+            {author.memberId === loginUser.memberId && (
+              <>
+                <StyledButton onClick={onEditClick}>수정</StyledButton>
+                <StyledButton onClick={handleOpenModal}>삭제</StyledButton>
+              </>
+            )}
           </ButtonContainer>
         </OwnerInfoContainer>
 
@@ -52,7 +70,7 @@ function CommentBox() {
       {/*TODO: 추후 전역 hooks 로 관리 (리팩토링)*/}
       {isOpen && (
         <Modal
-          title={'게시글을 삭제하시겠습니까?'}
+          title={'댓글을 삭제하시겠습니까?'}
           contents={'삭제한 내용은 복구 할 수 없습니다.'}
           handleClose={handleCloseModal}
           handleConfirm={handleConfirm}
@@ -64,7 +82,7 @@ function CommentBox() {
 
 export default CommentBox;
 
-const CommentInfoContainer = styled.div`
+const CommentInfoContainer = styled.li`
   display: flex;
   flex-direction: column;
 
@@ -95,6 +113,7 @@ const ButtonContainer = styled.div`
   justify-content: space-between;
 
   width: 100px;
+  min-height: 26px;
   margin-top: 30px;
   margin-left: auto;
 `;
--- src/components/CommentForm/index.jsx
+++ src/components/CommentForm/index.jsx
@@ -1,25 +1,73 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import S from '@/styles/common.jsx';
-import Button from '@/components/Button/index.jsx';
+import Button from '@/components/Button';
 
-function CommentForm() {
-  // TODO: Common Button 수정되면 변경
+CommentForm.propTypes = {
+  isEditing: PropTypes.bool,
+  setIsEditing: PropTypes.func, // TODO: 커스텀 훅 구현 후 props 에서 삭제
+  currentComment: PropTypes.object,
+};
+
+// TODO: Common Button 수정되면 변경
+function CommentForm({ isEditing, setIsEditing, currentComment }) {
+  console.debug('CommentForm() - rendering');
+
+  const [text, setText] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAddSubmitButton = (e) => {
+    e.preventDefault();
+
+    console.log(`새 댓글 추가 => ${text}`);
+  };
+
+  const handleEditSubmitButton = (e) => {
+    e.preventDefault();
+
+    console.log(`댓글 수정 => ${text}`);
+  };
+
+  useEffect(() => {
+    if (isEditing) {
+      setText(currentComment.contents);
+    }
+  }, [isEditing, currentComment]);
+
+  // 댓글 수정중 기존의 댓글을 모두 지우면, 댓글 작성으로 변경
+  useEffect(() => {
+    if (text.trim().length === 0) {
+      setIsDisabled(true);
+      setIsEditing(false);
+      return;
+    }
+
+    setIsDisabled(false);
+  }, [text]);
+
   return (
     <StyledForm>
-      <StyledTextarea />
+      <StyledTextarea onChange={handleTextChange} defaultValue={text} />
 
       <S.Hr />
 
-      <SummitButtonContainer>
+      <SubmitButtonContainer>
         <Button
           width={'90px'}
-          text={'댓글작성'}
-          type={'summit'}
+          text={isEditing ? '댓글수정' : '댓글작성'}
+          type={'submit'}
+          disabled={isDisabled}
+          onClick={isEditing ? handleEditSubmitButton : handleAddSubmitButton}
           $margin={'0 15px 0 0'}
           $radius={'8px'}
         />
-      </SummitButtonContainer>
+      </SubmitButtonContainer>
     </StyledForm>
   );
 }
@@ -48,7 +96,7 @@ const StyledTextarea = styled.textarea`
   outline: none;
 `;
 
-const SummitButtonContainer = styled.div`
+const SubmitButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 
