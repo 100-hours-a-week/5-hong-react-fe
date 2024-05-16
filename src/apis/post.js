e7dc9cbe020105723bb090ad3fcb38d48e913c2c
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-16 21:18:24 +0900

    feat: Add read update delete post apis

--- src/apis/post.js
+++ src/apis/post.js
@@ -11,9 +11,26 @@ export const getPostList = async (page) => {
   };
 };
 
+export const getPostDetail = async (postsId) => {
+  const url = `/api/v1/posts/${postsId}`;
+  return await api.get(url);
+};
+
 export const createPost = async (postInfo) => {
   const url = '/api/v1/posts';
   const data = { ...postInfo };
   const body = JSON.stringify(data);
   return await api.post(url, { body });
+};
+
+export const updatePost = async (postsId, postInfo) => {
+  const url = `/api/v1/posts/${postsId}`;
+  const data = { ...postInfo };
+  const body = JSON.stringify(data);
+  return await api.put(url, { body });
+};
+
+export const deletePost = async (postsId) => {
+  const url = `/api/v1/posts/${postsId}`;
+  return await api.delete(url);
 };
