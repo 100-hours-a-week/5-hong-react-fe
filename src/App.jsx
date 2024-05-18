377506e987e258db040df33b1efadfbd82010398
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-19 00:23:19 +0900

    feat: Add auth provider into app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Router from '@/router/Router.jsx';
+
 import GlobalStyles from '@/styles/GlobalStyles.jsx';
+import AuthProvider from '@/hooks/context/AuthProvider.jsx';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <GlobalStyles />
       <Router />
-    </>
+    </AuthProvider>
   );
 }
 
