02365870e2c3192acbfef1d2a0cfc27e33f40098
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-20 01:33:14 +0900

    refact: Update login components to use custom hook

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,78 +1,43 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 import Input from '@/components/Input';
 import Button from '@/components/Button';
-import REGEX from '@/constants/regex.js';
-import VALIDATE_MESSAGES from '@/constants/validateMessages.js';
 import PATH from '@/constants/path.js';
 
+import formValidate from '@/components/LoginForm/formValidate.js';
+import useForm from '@/hooks/useForm.js';
+import useAuth from '@/hooks/useAuth.js';
+import useToast from '@/hooks/useToast.js';
 import { loginUser } from '@/apis/user.js';
 
-// TODO: hook 으로 분리
 function LoginForm() {
   console.debug('LoginForm() - rendering');
 
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [emailHelperText, setEmailHelperText] = useState(
-    VALIDATE_MESSAGES.EMAIL.REQUIRED,
-  );
-  const [passwordHelperText, setPasswordHelperText] = useState(
-    VALIDATE_MESSAGES.PASSWORD.REQUIRED,
-  );
+  const createToast = useToast();
   const navigate = useNavigate();
+  const { reload } = useAuth();
+  const { isLoading, values, errors, handleOnChange, handleOnSubmit } = useForm(
+    {
+      initialValues: { email: '', password: '' },
+      onSubmit: () => loginRequest({ ...values }),
+      validateFn: (formValue) => formValidate(formValue),
+    },
+  );
 
-  const isSubmitDisabled = !email || !password;
-
-  const handleChangeEmail = (e) => {
-    const value = e.target.value;
-
-    if (value.trim().length === 0) {
-      setEmailHelperText(VALIDATE_MESSAGES.EMAIL.REQUIRED);
-      setEmail(null);
-      return;
-    }
-
-    if (!REGEX.EMAIL.test(value)) {
-      setEmailHelperText(VALIDATE_MESSAGES.EMAIL.INVALID);
-      setEmail(null);
-      return;
-    }
-
-    setEmailHelperText(null);
-    setEmail(value);
-  };
-
-  const handleChangePassword = (e) => {
-    const value = e.target.value;
-
-    if (value.trim().length === 0) {
-      setPasswordHelperText(VALIDATE_MESSAGES.PASSWORD.REQUIRED);
-      setPassword(null);
-      return;
-    }
-
-    if (!REGEX.PASSWORD.test(value)) {
-      setPasswordHelperText(VALIDATE_MESSAGES.PASSWORD.INVALID);
-      setPassword(null);
-      return;
-    }
-
-    setPasswordHelperText(null);
-    setPassword(value);
-  };
-
-  const handleSubmitButton = async (e) => {
-    e.preventDefault();
-
-    await loginUser({ email, password })
-      .then(() => navigate(PATH.MAIN))
+  const loginRequest = async (loginInfo) => {
+    await loginUser(loginInfo)
+      .then(() => {
+        navigate(PATH.MAIN);
+        createToast({ message: '로그인 성공' });
+        reload();
+      })
       .catch(() => {
-        setPasswordHelperText('*비밀번호를 확인해주세요.');
+        createToast({ message: '이메일 혹은 비밀번호 오류' });
       });
   };
+
+  const isSubmitDisabled = Object.keys(errors).length > 0 || isLoading;
 
   return (
     <StyledLoginForm>
@@ -80,22 +45,24 @@ function LoginForm() {
         <Input
           id={'email'}
           type={'text'}
+          name={'email'}
           label={'이메일'}
           required={true}
           placeholder={'이메일을 입력해주세요.'}
-          onChange={handleChangeEmail}
-          helperText={emailHelperText}
+          onChange={handleOnChange}
+          helperText={errors.email}
         />
       </StyledInputContainer>
       <StyledInputContainer>
         <Input
           id={'password'}
           type={'password'}
+          name={'password'}
           label={'비밀번호'}
           required={true}
           placeholder={'비밀번호를 입력해주세요.'}
-          onChange={handleChangePassword}
-          helperText={passwordHelperText}
+          onChange={handleOnChange}
+          helperText={errors.password}
         />
       </StyledInputContainer>
 
@@ -104,7 +71,7 @@ function LoginForm() {
         text={'로그인'}
         width={'100%'}
         disabled={isSubmitDisabled}
-        onClick={handleSubmitButton}
+        onClick={handleOnSubmit}
         $margin={'15px 0 0'}
       />
     </StyledLoginForm>
