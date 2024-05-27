21071b4fe39de3cf2f27dd1802572f71e2b653e5
JiHongKim98 <kinjihong9598@gmail.com>
2024-05-27 19:55:37 +0900

    refact: Update password form to use custom hooks

--- src/components/PasswordForm/index.jsx
+++ src/components/PasswordForm/index.jsx
@@ -1,87 +1,41 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 import Input from '@/components/Input';
 import Button from '@/components/Button';
-import VALIDATE_MESSAGES from '@/constants/validateMessages.js';
-import REGEX from '@/constants/regex.js';
 import PATH from '@/constants/path.js';
 
+import formValidate from '@/components/PasswordForm/formValidate.js';
+import useForm from '@/hooks/useForm.js';
+import useToast from '@/hooks/useToast.js';
 import { updatePassword } from '@/apis/user.js';
 
 function PasswordForm() {
-  const [password, setPassword] = useState(null);
-  const [passwordHelperText, setPasswordHelperText] = useState(
-    VALIDATE_MESSAGES.PASSWORD.REQUIRED,
-  );
-  const [passwordConfirm, setPasswordConfirm] = useState(null);
-  const [passwordConfirmHelperText, setPasswordConfirmHelperText] = useState(
-    VALIDATE_MESSAGES.PASSWORD_CONFIRM.REQUIRED,
-  );
+  console.debug('PasswordForm() - rendering');
+
+  const createToast = useToast();
   const navigate = useNavigate();
-
-  // 비밀번호 유효성 검사 이벤트
-  const handleChangePassword = (e) => {
-    const value = e.target.value;
-
-    if (value.trim().length === 0) {
-      setPasswordHelperText(VALIDATE_MESSAGES.PASSWORD.REQUIRED);
-      setPassword(null);
-      return;
-    }
-
-    if (!REGEX.PASSWORD.test(value)) {
-      setPasswordHelperText(VALIDATE_MESSAGES.PASSWORD.INVALID);
-      setPassword(null);
-      return;
-    }
-
-    setPasswordHelperText(null);
-    setPassword(value);
-  };
-
-  // 비밀번호 확인 유효성 검사 이벤트
-  const handleChangePasswordConfirm = (e) => {
-    const value = e.target.value;
-
-    if (value.trim().length === 0) {
-      setPasswordConfirmHelperText(VALIDATE_MESSAGES.PASSWORD_CONFIRM.REQUIRED);
-      setPasswordConfirm(null);
-      return;
-    }
-
-    if (value !== password) {
-      setPasswordConfirmHelperText(VALIDATE_MESSAGES.PASSWORD_CONFIRM.MISMATCH);
-      setPasswordConfirm(null);
-      return;
-    }
-
-    setPasswordConfirmHelperText(null);
-    setPasswordConfirm(value);
-  };
-
-  useEffect(() => {
-    if (password && password === passwordConfirm) {
-      setPasswordConfirmHelperText(null);
-      return;
-    }
-    setPasswordConfirmHelperText(VALIDATE_MESSAGES.PASSWORD_CONFIRM.MISMATCH);
-  }, [password, passwordConfirm]);
+  const { values, errors, isLoading, handleOnChange, handleOnSubmit } = useForm(
+    {
+      initialValues: {
+        password: '',
+        passwordConfirm: '',
+      },
+      onSubmit: () => updatePasswordRequest({ ...values }),
+      validateFn: formValidate,
+    },
+  );
 
   // 버튼 이벤트
-  const isSubmitDisabled = !!passwordHelperText || !!passwordConfirmHelperText;
+  const isSubmitDisabled = Object.keys(errors).length > 0 || isLoading;
 
-  const handleSubmitButton = async (e) => {
-    e.preventDefault();
-
-    console.log('누름');
-    console.log('password =', password);
-    console.log('password confirm =', passwordConfirm);
-
-    await updatePassword({ password })
-      .then(() => navigate(PATH.MAIN))
-      .catch(() => console.log('비밀번호 업데이트 실패'));
+  const updatePasswordRequest = async (updateInfo) => {
+    await updatePassword(updateInfo)
+      .then(() => {
+        navigate(PATH.MAIN);
+        createToast({ message: '비밀번호 변경 완료' });
+      })
+      .catch(() => createToast({ message: '비밀번호 변경 실패' }));
   };
 
   return (
@@ -90,29 +44,31 @@ function PasswordForm() {
         <Input
           id={'password'}
           type={'password'}
+          name={'password'}
           label={'비밀번호'}
           required={true}
           placeholder={'비밀번호를 입력해주세요.'}
-          onChange={handleChangePassword}
-          helperText={passwordHelperText}
+          onChange={handleOnChange}
+          helperText={errors.password}
         />
       </StyledInputContainer>
       <StyledInputContainer>
         <Input
           id={'password-confirm'}
           type={'password'}
+          name={'passwordConfirm'}
           label={'비밀번호 확인'}
           required={true}
           placeholder={'비밀번호를 입력해주세요.'}
-          onChange={handleChangePasswordConfirm}
-          helperText={passwordConfirmHelperText}
+          onChange={handleOnChange}
+          helperText={errors.passwordConfirm}
         />
       </StyledInputContainer>
 
       <Button
         type={'submit'}
         text={'수정하기'}
-        onClick={handleSubmitButton}
+        onClick={handleOnSubmit}
         disabled={isSubmitDisabled}
         width={'100%'}
         $margin={'15px 0 0'}
