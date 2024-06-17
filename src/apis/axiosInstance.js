895b07541a9d90086e5c9b27ae892ba75c716b93
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-17 12:02:45 +0900

    feat: Add axios interceptor to handle token error response

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
 import { API_BASE_URL } from '@/constants/baseConfig.js';
+import { handleTokenError } from '@/apis/interceptors.js';
+
+// TODO: 상수 관리
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 5000, // 5초
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  handleTokenError, // 응답 에러는 handleTokenError 에서 처리
+);
