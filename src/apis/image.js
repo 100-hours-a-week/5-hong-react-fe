21a805d96d8e209a855a19402ea6d78a63113cde
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-18 10:57:15 +0900

    chore: Removed image api that uses fetch

--- src/apis/image.js
+++ src/apis/image.js
@@ -1,8 +1,6 @@
-import api from '@/apis/core';
-
-export const uploadImage = async (image) => {
-  const url = '/api/v1/uploads/image';
-  const formData = new FormData();
-  formData.append('file', image);
-  return await api.post(url, { body: formData });
-};
+// export const uploadImage = async (image) => {
+//   const url = '/api/v1/uploads/upload';
+//   const formData = new FormData();
+//   formData.append('file', image);
+//   return await api.post(url, { body: formData });
+// };
